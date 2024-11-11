fd46b975a438bd3f5412be98ea261180b8e0d1ea
Tarun Sharma <59308544+tarun2001sharma@users.noreply.github.com>
2024-11-10 20:22:37 -0500

    Update course-schedule.py

--- course-schedule.py
+++ course-schedule.py
@@ -6,30 +6,74 @@ class Solution:
     Space Complexity:
     O(m + n) for the adjacency list, indegree array, and queue.
     '''
+    # def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+    #     indegree= [0]*numCourses
+    #     adj_list = []
+    #     for _ in range(numCourses):
+    #         adj_list.append([])
+
+    #     for post, pre in prerequisites:
+    #         indegree[post] += 1
+    #         adj_list[pre].append(post)
+        
+    #     queue = deque()
+    #     for idx in range(len(indegree)):
+    #         if indegree[idx] == 0:
+    #             queue.append(idx)
+
+    #     nodes_visited = 0
+    #     while queue:
+    #         curr = queue.popleft()
+    #         nodes_visited += 1
+
+    #         for neigh in adj_list[curr]:
+    #             indegree[neigh]-=1
+    #             if indegree[neigh] == 0:
+    #                 queue.append(neigh)
+        
+    #     return nodes_visited == numCourses
+
+    '''
+    DFS Approach to detect cycles
+    O(V+E) time and space complexity
+    '''
+
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        indegree= [0]*numCourses
-        adj_list = []
-        for _ in range(numCourses):
-            adj_list.append([])
-
-        for post, pre in prerequisites:
-            indegree[post] += 1
-            adj_list[pre].append(post)
+        graph = defaultdict(list)
+        for course, prereq in prerequisites:
+            graph[prereq].append(course)
         
-        queue = deque()
-        for idx in range(len(indegree)):
-            if indegree[idx] == 0:
-                queue.append(idx)
+        visited = [0] * numCourses
+        # 0 - not visited
+        # 1 - visiting currently, will only be seen in backtrack
+        # 2 - visited
 
-        nodes_visited = 0
-        while queue:
-            curr = queue.popleft()
-            nodes_visited += 1
+        def dfs(node):
+            if visited[node] == 1:
+                return False
+            if visited[node] == 2:
+                return True
+            
+            # visiting
+            visited[node] = 1
 
-            for neigh in adj_list[curr]:
-                indegree[neigh]-=1
-                if indegree[neigh] == 0:
-                    queue.append(neigh)
+            # recurse to all neightbours
+            for neigh in graph[node]:
+                if not dfs(neigh):
+                    return False
+            
+            visited[node] = 2
+            return True
         
-        return nodes_visited == numCourses
+        for course in range(numCourses):
+            if visited[course] == 0: #unvisited - then perform dfs
+                if not dfs(course):
+                    return False
+        
+        return True
+
+
+
+
+
         
